9f5d8c35e4c727a20eaabbe860b4dfa32735d573
jogetyoung <youngjoeljj@gmail.com>
2025-01-09 11:44:43 +0800

    first

---
+++ src/test/java/com/example/paf_day1/PafDay1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.paf_day1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PafDay1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
